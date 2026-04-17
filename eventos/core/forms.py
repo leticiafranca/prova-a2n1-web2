0d47200f3c1d602b5ceb1987453d1b95012a4cca
Letícia França <leticiacavalcanted.franca@gmail.com>
2026-04-17 14:22:30 -0300

    Delete eventos directory

--- eventos/core/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class EventoForm(forms.Form):
-    nome = forms.CharField(label="Evento", max_length=100)
-    local = forms.CharField(label="Local", max_length=100)
--- eventos/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('novo_evento/', views.novo_evento, name='novo'),
-]
